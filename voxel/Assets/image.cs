5ef3c301f3e89df081e93e04d13db3f78f0f6f8b
LiBo <libo123459@126.com>
2017-07-22 14:06:01 +0800

    voxel

---
+++ voxel/Assets/image.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class image : MonoBehaviour {
+    Material mat;
+    private void OnTriggerEnter(Collider collision)
+    {
+        collision.transform.DOScale(0.2f,0.2f);
+        // mat = collision.GetComponent<MeshRenderer>().material;
+        //collision.GetComponent<MeshRenderer>().material = Resources.Load<Material>("1");
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        collision.transform.DOScale(0f,0.2f);
+        //collision.GetComponent<MeshRenderer>().material = mat;
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        //collision.GetComponent<MeshRenderer>().material = Resources.Load<Material>("1");
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localRotation *= Quaternion.Euler(0, 30 * Time.deltaTime, 30 * Time.deltaTime);
+    }
+}
---
+++ voxel/Assets/manage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class manage : MonoBehaviour {
+    public GameObject block;
+    public Transform grid;
+    public List<GameObject> blocklist = new List<GameObject>();
+    float coe = 0.2f;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                for (int k = 0; k < 20; k++)
+                {
+                    GameObject _block = Instantiate(block,grid);
+                    _block.transform.localPosition = new Vector3(coe * i,coe * j,coe * k);
+                    blocklist.Add(_block);
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
